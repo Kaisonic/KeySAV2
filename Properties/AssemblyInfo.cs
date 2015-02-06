985cb2ab96e47e2887595cc201046a72f4e50788
Kaisonic <github@kaisonic.net>
2015-02-05 22:41:27 -0500

    Don't Need Readme Anymore

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KeySAV 2")]
-[assembly: AssemblyDescription("Data Decrypter by Kaphotics")]
+[assembly: AssemblyTitle("KeySAV3")]
+[assembly: AssemblyDescription("Data Decrypter by Kaphotics; Kaisonic's Build")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ProjectPokemon.org")]
-[assembly: AssemblyProduct("KeySAV 2")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("KeySAV3")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
